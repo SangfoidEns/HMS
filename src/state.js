42ac93b1b6d891b9bb01df874fa399200c03fdc9
SangfoidEns <90978826+SangfoidEns@users.noreply.github.com>
2026-08-21 18:11:26 +0200

    Delete src directory

--- src/state.js
+++
@@ -1,9 +0,0 @@
-export const state = {
-  procurementCosts: JSON.parse(localStorage.getItem('h2_procurement_costs')) || { 'BANNAN': 600 },
-  parsedRecords: [],
-  charts: { weight: null, revenue: null }
-};
-
-export function saveState() {
-  localStorage.setItem('h2_procurement_costs', JSON.stringify(state.procurementCosts));
-}
